4aa036938f26a2dedbe7f71cb2c760e7f531c1ea
md.salman masud <mdsalmanraselmasud@gmail.com>
2020-10-25 20:58:09 +0600

    added some modify in hello.java

--- hello.java
+++ hello.java
@@ -1,9 +1,10 @@
 public class hello {
     public static void main(string[] args){
         system.out.printIn("vs code is awsome..");
-        gretting("salman masud")
+        gretting("*Git")
     }
 
  public static void gretting(string name){
-    system.out.println("welcome"+ name);
+    system.out.println("welcome to"+ name);
  }
+
